3da64ba49455a587d2ac85eab218a4264df615b2
ranjeetkumbhar01 <kumbhar.ranjeet01@gmail.com>
2024-04-21 00:49:33 +0530

    add: Assignment 5

---
+++ Assignment 5/TokenRing.java
@@ -0,0 +1,65 @@
+/*
+To run
+javac TokenRing.java
+java TokenRing
+*/
+
+import java.util.*;
+
+public class TokenRing {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Getting the number of nodes in the ring
+        System.out.print("Enter the number of nodes you want in the ring: ");
+        int n = sc.nextInt();
+
+        // Displaying the ring formation
+        System.out.println("Ring Formed is as below: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(i + " ");
+        }
+        // System.out.println("0");
+
+        int choice = 0;
+        do {
+            // Getting sender, receiver, and data
+            System.out.print("\nEnter Sender: ");
+            int sender = sc.nextInt();
+            System.out.print("Enter Receiver: ");
+            int receiver = sc.nextInt();
+            System.out.print("Enter Data to Send: ");
+            int data = sc.nextInt();
+            int token = 0;
+            int i = 0;
+            // System.out.println(" " + sender);
+            System.out.println("Sender: " + sender + " Sending Data: " + data);
+
+            // Token Passing
+            System.out.println("\nToken Passing: [Current Token At: " + token + " ]");
+            for (i = token; i < sender; i++) {
+                System.out.print(" " + i + " ->");
+            }
+
+            // Forwarding data until it reaches the receiver
+            for (i = sender; i != receiver; i = (i + 1) % n) {
+                // System.out.println("Data Forwarded by: " + i);
+                System.out.print(" " + i + " ->");
+            }
+            System.out.print(" " + receiver);
+            System.out.println("\nReceiver: " + receiver + ", Received the data: " + data);
+            token = sender;
+            if (i < n - 1) {
+                for (; i < n; i++)
+                    System.out.print(" " + i + " ->");
+
+            }
+            // Asking user if they want to send data again
+            System.out.print("\nDo you want to send data again? If yes Enter 1, If no Enter 0: ");
+            choice = sc.nextInt();
+        } while (choice == 1);
+
+        // Closing scanner
+        sc.close();
+    }
+}
